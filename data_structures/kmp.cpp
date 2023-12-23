e073d134c6fbc1cab69f1f70e5103487c304d0c9
LukeZengf <zenglukef@gmail.com>
2023-12-22 16:31:06 -0800

    finished and tested kmp

--- data_structures/kmp.cpp
+++ data_structures/kmp.cpp
@@ -1,13 +1,40 @@
 #include <bits/stdc++.h>
 
+#define forn(i,n) for (int i = 0; i < n; i++)
+
 using namespace std;
 
+
+
+template<typename A> ostream& operator<< (ostream &cout, vector<A> const&v) {
+    cout << "["; forn(i,(int)v.size()){ if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+
+
+
 vector<int> kmp(string& s) {
-    vector<int> output;
-    
+
+    int n = s.size();
+
+    int fail = 0;
+    vector<int> output(n,0);
+
+    for (int i = 1; i < n; i++) {
+        while (fail > 0 && s[i] != s[fail]) {
+            fail = output[fail - 1];
+        }
+        if (s[i] == s[fail])
+            fail++;
+        output[i] = fail;
+    }
+
+    return output;
 }
 
 int main() {
+
+    string f = "abcabcabcaaabbccc";
+    cout << kmp(f);
 
     return 0;
 }
