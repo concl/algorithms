3d1993d11a596f3f6159b1f4091550a2ac113ad1
LukeZengf <zenglukef@gmail.com>
2024-04-07 00:01:37 -0700

    .

--- problems/leetcode/3108.cpp
+++ problems/leetcode/3108.cpp
@@ -14,8 +14,11 @@ int find(vector<int>& uf, int a) {
 
 void union_sets(vector<int>& uf, vector<long long>& sizes, vector<int>& rep, int a, int b, int weight) {
 
+    // ap and bp are the parents of a and b
     int ap = find(uf, a);
     int bp = find(uf, b);
+
+    // edge case where a and b are already in the same component
     if (ap == bp) {
         rep[ap] &= weight;
         return;
@@ -28,11 +31,14 @@ void union_sets(vector<int>& uf, vector<long long>& sizes, vector<int>& rep, int
     sizes[ap] += sizes[bp];
 
     if ((rep[ap] != -1) && (rep[bp] != -1)) {
+        // if both a and b have more than 1 node then the bitwise and will be the bitwise and of all the edges in both components, and the edge connecting them
         rep[ap] &= rep[bp];
         rep[ap] &= weight;
     } else if ((rep[ap] == -1) && (rep[bp] == -1)) {
+        // if both a and b have only 1 node then the bitwise and will only be the edge weight connecting them
         rep[ap] = weight;
     } else {
+        // b is a single node, so we update the bitwise and of the connected component including a with the edge weight
         rep[ap] &= weight;
     }
 }
@@ -41,7 +47,11 @@ class Solution {
 public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
         
+        // stores the bitwise and of all the edges in a connected component.
+        // -1 means there is only 1 node.
         vector<int> rep(n,-1);
+
+        // standard union find
         vector<long long> sizes(n,1);
         vector<int> uf(n);
         for (int i = 0; i < n; i++) {
@@ -49,10 +59,12 @@ public:
         }
 
         for (auto x : edges) {
+            // modified union sets
             union_sets(uf, sizes, rep, x[0], x[1], x[2]);
         }
 
-
+        // for each query, check if the nodes are in the same connected component.
+        // if they are, then return the bitwise and of all the edges in the connected component.
         vector<int> out;
         for (auto x : query) {
             if (x[0] == x[1]) {
