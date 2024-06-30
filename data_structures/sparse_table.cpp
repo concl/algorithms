10461f4373087c4edc1356cde8a45f9cc90d8169
LukeZengf <zenglukef@gmail.com>
2024-06-30 12:40:30 -0700

    sparse table

--- data_structures/sparse_table.cpp
+++ data_structures/sparse_table.cpp
@@ -8,12 +8,14 @@ int log2_floor(unsigned long long x) {
     return x ? __builtin_clzll(1) - __builtin_clzll(x) : -1;
 }
 
-// minimum element in range [l, r)
+// min or max element in range [l, r]
 class SparseTable {
 
 public:
     vector<vector<int>> st;
-    SparseTable(vector<int> &a) {
+    function<int(int, int)> f;
+    SparseTable(vector<int> &a, function<int(int, int)> f) {
+        this->f = f;
 
         int n = a.size();
         int k = log2_floor(n) + 1;
@@ -25,15 +27,14 @@ public:
 
         for (int j = 1; j < k; j++) {
             for (int i = 0; i + (1 << j) <= n; i++) {
-                st[j][i] = min(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+                st[j][i] = f(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
             }
         }
-
     }
 
     int query(int l, int r) {
         int j = log2_floor(r - l + 1);
-        return min(st[j][l], st[j][r - (1 << j) + 1]);
+        return f(st[j][l], st[j][r - (1 << j) + 1]);
     }
 
 };
