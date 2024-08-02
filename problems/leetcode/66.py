9d28034aa323b458af99f25a52f8446b6b113f4d
LukeZengf <zenglukef@gmail.com>
2024-08-02 13:51:02 -0700

    66

---
+++ problems/leetcode/66.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits):
+        n = len(digits)
+        output = digits[:]
+        for i in range(n - 1, -1, -1):
+            output[i] += 1
+            if output[i] < 10:
+                break
+            else:
+                output[i] = 0
+        if output[0] == 0:
+            output.insert(0, 1)
+        return output
