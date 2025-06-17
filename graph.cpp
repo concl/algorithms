6d2763a6c7b1c60df1c63fa313633bd4f214b6c3
LukeZengf <zenglukef@gmail.com>
2025-06-17 01:58:12 -0700

    math rust

--- graph.cpp
+++ graph.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-bool bfs(vector<vector<int>> &graph, int source, int target) {
+bool bfs(vector<vector<int>>& graph, int source, int target) {
     unordered_set<int> seen;
     queue<int> q;
     q.push(source);
@@ -27,7 +27,7 @@ bool bfs(vector<vector<int>> &graph, int source, int target) {
     return false; // No path found
 }
 
-bool dfs(vector<vector<int>> &graph, int source, int target) {
+bool dfs(vector<vector<int>>& graph, int source, int target) {
 
     unordered_set<int> seen;
     vector<int> stack;
@@ -50,6 +50,19 @@ bool dfs(vector<vector<int>> &graph, int source, int target) {
     }
 
     return false;
+}
+
+// Constructs an undirected graph, given edges
+vector<vector<int>> construct_graph(int n, vector<vector<int>>& edges) {
+
+    vector<vector<int>> graph(n);
+
+    for (const auto& edge : edges) {
+        graph[edge[0]].push_back(edge[1]);
+        graph[edge[1]].push_back(edge[0]);
+    }
+
+    return graph;
 }
 
 
---
+++ rust_algorithms/src/math/combinatorics.rs
@@ -0,0 +1,43 @@
+
+
+const MOD: i64 = 1_000_000_007;
+
+fn pow_mod(base: i64, exp: i64, modulus: i64) -> i64 {
+    let mut res = 1;
+    let mut b = base % modulus;
+    let mut e = exp;
+
+    while e > 0 {
+        if e % 2 == 1 {
+            res = (res * b) % modulus;
+        }
+        b = (b * b) % modulus;
+        e /= 2;
+    }
+    res
+}
+
+fn inv(a: i64) -> i64 {
+    pow_mod(a, MOD - 2, MOD)
+}
+
+fn factorial(factorial_table: &mut Vec<i64>, n: usize) {
+    if factorial_table.len() == 0 {
+        factorial_table.push(1); // 0! = 1
+    }
+
+    for i in factorial_table.len()..=n {
+        factorial_table.push((factorial_table[i - 1] * i as i64) % MOD);
+    }
+    factorial_table[n];
+}
+
+fn comb(factorial_table: &mut Vec<i64>, n: usize, k: usize) -> i64 {
+    if k > n || k < 0 {
+        return 0;
+    }
+    factorial(factorial_table, n);
+    let numerator = factorial_table[n];
+    let denominator = (factorial_table[k] * factorial_table[n - k]) % MOD;
+    (numerator * inv(denominator)) % MOD
+}
