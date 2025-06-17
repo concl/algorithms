7872fb0078fdfad81de9280284ae63b83859869a
LukeZengf <zenglukef@gmail.com>
2025-06-17 02:07:59 -0700

    combinatorics

--- rust_algorithms/src/math/combinatorics.rs
+++ rust_algorithms/src/math/combinatorics.rs
@@ -1,5 +1,6 @@
 
 
+
 const MOD: i64 = 1_000_000_007;
 
 fn pow_mod(base: i64, exp: i64, modulus: i64) -> i64 {
@@ -21,7 +22,7 @@ fn inv(a: i64) -> i64 {
     pow_mod(a, MOD - 2, MOD)
 }
 
-fn factorial(factorial_table: &mut Vec<i64>, n: usize) {
+fn factorial(factorial_table: &mut Vec<i64>, n: usize) -> i64 {
     if factorial_table.len() == 0 {
         factorial_table.push(1); // 0! = 1
     }
@@ -29,15 +30,15 @@ fn factorial(factorial_table: &mut Vec<i64>, n: usize) {
     for i in factorial_table.len()..=n {
         factorial_table.push((factorial_table[i - 1] * i as i64) % MOD);
     }
-    factorial_table[n];
+    factorial_table[n]
 }
 
 fn comb(factorial_table: &mut Vec<i64>, n: usize, k: usize) -> i64 {
     if k > n || k < 0 {
         return 0;
     }
-    factorial(factorial_table, n);
-    let numerator = factorial_table[n];
-    let denominator = (factorial_table[k] * factorial_table[n - k]) % MOD;
+
+    let numerator = factorial(factorial_table, n);
+    let denominator = factorial_table[k] * factorial_table[n - k] % MOD;
     (numerator * inv(denominator)) % MOD
 }
