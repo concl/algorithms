8a28600172d7f8385459bffef00137662d3705ab
concl <107962474+concl@users.noreply.github.com>
2025-08-06 01:13:18 -0700

    added fenwick tree, hld coming soon

--- data_structures/fenwick_tree.cpp
+++ data_structures/fenwick_tree.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+
+class FenwickTree {
+
+public:
+    vector<int> tree;
+    int n;
+
+    FenwickTree(int size, vector<int> &arr) : n(size) {
+        tree.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            update(i, arr[i]);
+        }
+    }
+
+    void update(int idx, int val) {
+        for (; idx < n; idx |= idx + 1) {
+            tree[idx] += val;
+        }
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        for (; idx >= 0; idx = (idx & (idx + 1)) - 1) {
+            sum += tree[idx];
+        }
+        return sum;
+    }
+};
+
+
 int main() {
     
     
