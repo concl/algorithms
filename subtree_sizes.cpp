142175c441e7b841ed05db7b310d4154012f43c6
concl <bonumconcl@gmail.com>
2025-11-14 15:11:02 -0800

    subtree_sizes

--- subtree_sizes.cpp
+++ subtree_sizes.cpp
@@ -2,25 +2,29 @@
 
 using namespace std;
 
+typedef pair<int,int> pii;
 
-vector<vector<int>> subtree_sizes(vector<vector<int>>& adj) {
+// given an adjacency list for a graph, for each node,
+// treats it as a root and finds the size of the subtree
+// rooted at each child.
+vector<vector<pii>> subtree_sizes(vector<vector<int>>& adj) {
     int n = adj.size();
-    vector<int> size(n, 1);
-    vector<vector<int>> subtree(n);
-    for (int i = 0; i < n; i++) {
-        subtree[i].push_back(i);
-    }
-    function<void(int, int)> dfs = [&](int u, int p) {
-        for (int v : adj[u]) {
-            if (v == p) continue;
-            dfs(v, u);
-            size[u] += size[v];
-            for (int x : subtree[v]) {
-                subtree[u].push_back(x);
-            }
+    vector<vector<pii>> subtree(n);
+
+    function<int(int, int)> dfs1 = [&](int node, int parent) {   
+        int res = 1;
+        for (int v : adj[node]) {
+            if (v == parent) continue;
+            int child_size = dfs1(v, node);
+            res += child_size;
+            subtree[node].push_back({v, child_size});
         }
+        if (parent != -1) {
+            subtree[node].push_back({parent, n - res});
+        }
+        return res;
     };
-    dfs(0, -1);
+    dfs1(0, -1);
     return subtree;
 }
 
