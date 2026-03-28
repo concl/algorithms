b320cc2ee31631158d0c06986e633685ec9d78a1
LukeZengf <bonumconcl@gmail.com>
2026-03-27 20:14:18 -0700

    minor

--- run_template_tests.py
+++ run_template_tests.py
@@ -2,8 +2,14 @@
 import subprocess
 import os
 
-path = os.path.dirname(__file__)
-tests_path = os.path.join(path, "tests/")
+PATH = os.path.dirname(__file__)
+TESTS = os.path.join(PATH, "tests/")
 
-for (dirpath, dirnames, filenames) in os.walk(tests_path):
-    pass
+for (dirpath, dirnames, filenames) in os.walk(TESTS):
+    for filename in filenames:
+        if filename.endswith(".py") and not filename.startswith("test_"):
+            continue
+        if filename.endswith(".py") and filename.startswith("test_"):
+            print(f"Running {filename}...")
+            subprocess.run(["python", os.path.join(dirpath, filename)], check=True)
+
--- tests/test_segment_tree.cpp
+++ tests/test_segment_tree.cpp
@@ -1,5 +1,5 @@
 
-#include "../data_structures/segment_tree.cpp"
+#include "../cpp/data_structures/segment_tree.cpp"
 #include <cassert>
 
 void test_segment_tree() {
