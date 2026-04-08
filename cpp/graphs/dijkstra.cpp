6a7a8d074bdb43f0370de9d44d998e76cac52e00
concl <107962474+concl@users.noreply.github.com>
2026-04-07 23:00:20 -0700

    dijkstra's

---
+++ cpp/graphs/dijkstra.cpp
@@ -0,0 +1,52 @@
+/**
+ * Author: concl
+ * Status: Tested on CSES
+ * Description: Uses Dijkstra's algorithm to find the distance of the shortest path between
+ * a given node and all other nodes in a graph. For nodes that are unreachable, the distance
+ * is set to be -1
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+/**
+ * graph[i] stores all neighbors to node i, graph[i][j] is the weight of the edge between
+ * node i and j.
+ *
+ * Note that graph uses a map, replace this with a vector of pairs, or hash map for speed.
+ */
+vector<ll> shortest_distances(vector<map<int, ll>> &graph, int start) {
+
+    int n = graph.size();
+
+    priority_queue<
+        pair<ll, int>,
+        vector<pair<ll, int>>,
+        greater<pair<ll, int>>>
+        pq;
+
+    pq.push({0, start});
+
+    vector<ll> distances(n, -1);
+
+    while (pq.size() > 0) {
+
+        auto [dist, node] = pq.top();
+        pq.pop();
+
+        if (distances[node] != -1)
+            continue;
+        distances[node] = dist;
+
+        for (const auto &[nbr, weight] : graph[node]) {
+            if (distances[nbr] == -1) {
+                pq.push({dist + weight, nbr});
+            }
+        }
+    }
+
+    return distances;
+}
