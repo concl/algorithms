de2f6d712be6f738285a8a3493ba6dd9fb003d52
concl <107962474+concl@users.noreply.github.com>
2026-04-07 23:18:45 -0700

    more dijkstra

--- cpp/graphs/dijkstra.cpp
+++ cpp/graphs/dijkstra.cpp
@@ -1,9 +1,6 @@
 /**
  * Author: concl
  * Status: Tested on CSES
- * Description: Uses Dijkstra's algorithm to find the distance of the shortest path between
- * a given node and all other nodes in a graph. For nodes that are unreachable, the distance
- * is set to be -1
  */
 
 #include <bits/stdc++.h>
@@ -13,6 +10,10 @@ using namespace std;
 using ll = long long;
 
 /**
+ * Description: Uses Dijkstra's algorithm to find the distance of the shortest path between
+ * a given node and all other nodes in a graph. For nodes that are unreachable, the distance
+ * is set to be -1
+ * 
  * graph[i] stores all neighbors to node i, graph[i][j] is the weight of the edge between
  * node i and j.
  *
@@ -49,4 +50,42 @@ vector<ll> shortest_distances(vector<map<int, ll>> &graph, int start) {
     }
 
     return distances;
+}
+
+/**
+ * Description: Finds the length of the shortest path between start and target.
+ */
+ll distance(vector<map<int, ll>> &graph, int start, int target) {
+
+    int n = graph.size();
+    
+    unordered_set<int> seen;
+    priority_queue<
+        pair<ll, int>,
+        vector<pair<ll, int>>,
+        greater<pair<ll, int>>>
+        pq;
+
+    pq.push({0, start});
+
+    while (pq.size() > 0) {
+        
+        auto [dist, node] = pq.top();
+        pq.pop();
+
+        if (seen.find(node) != seen.end()) {
+            continue;
+        } else if (node == target) {
+            return dist;
+        }
+        seen.insert(node);
+
+        for (const auto &[nbr, weight] : graph[node]) {
+            if (seen.find(nbr) == seen.end()) {
+                pq.push({dist + weight, nbr});
+            }
+        }
+    }
+
+    return -1;
 }
