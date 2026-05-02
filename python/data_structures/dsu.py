ab96f539fcd8e2d4a08723bb49d51ee22907b238
concl <107962474+concl@users.noreply.github.com>
2026-05-01 20:17:38 -0700

    dsu python

--- python/data_structures/dsu.py
+++ python/data_structures/dsu.py
@@ -1,5 +1,26 @@
 
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        
+    
+    def find(self, a: int) -> int:
+        if self.parent[a] != a:
+            self.parent[a] = self.find(self.parent[a])
+        return self.parent[a]
 
+    def union_sets(self, a: int, b: int):
+        a = self.find(a)
+        b = self.find(b)
+        if a == b:
+            return
+        if self.size[a] < self.size[b]:
+            a, b = b, a
+        self.parent[b] = a
+        self.size[a] += self.size[b]
+
+# Procedural implementation
 def find(parent, a):
     if parent[a] != a:
         parent[a] = find(parent, parent[a])
